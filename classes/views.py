2feb10f48ca6148d8ef995e79685632cc605064c
LukaszPasich <lukas.zed81@gmail.com>
2021-11-15 20:47:46 +0000

    FEAT: Finish classes times in classes page

--- classes/views.py
+++ classes/views.py
@@ -15,10 +15,18 @@ def all_classes(request):
 
     mon_classes = Class.objects.all()[:8]
     tue_classes = Class.objects.all()[8:16]
+    wed_classes = Class.objects.all()[16:24]
+    thu_classes = Class.objects.all()[24:32]
+    fri_classes = Class.objects.all()[32:40]
+    sat_classes = Class.objects.all()[40:48]
 
     context = {
         'mon_classes': mon_classes,
         'tue_classes': tue_classes,
+        'wed_classes': wed_classes,
+        'thu_classes': thu_classes,
+        'fri_classes': fri_classes,
+        'sat_classes': sat_classes,
     }
 
     return render(request, 'classes/times.html', context)
