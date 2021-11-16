88a6043d22456514cf6fb8b5e2a9af6c8d8289db
LukaszPasich <lukas.zed81@gmail.com>
2021-11-16 04:53:36 +0000

    FEAT: Add contact view, urls and template

--- anaconda/urls.py
+++ anaconda/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('memberships/', include('membership.urls')),
     path('classes/', include('classes.urls')),
+    path('contact/', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ contact/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def contact(request):
+    """ Contact page view """
+
+    return render(request, 'contact/contact.html')
