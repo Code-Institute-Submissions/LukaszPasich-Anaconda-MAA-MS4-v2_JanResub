6b8827d8cf1a2b29c50996a722616bc25a313ee2
LukaszPasich <lukas.zed81@gmail.com>
2021-11-28 18:05:46 +0000

    MAINT: Move local database to postgres

--- anaconda/settings.py
+++ anaconda/settings.py
@@ -13,6 +13,8 @@ import os
 
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -127,7 +129,6 @@ DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-            # 'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 
